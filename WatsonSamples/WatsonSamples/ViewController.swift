dda1e6b9af2602a587434190fbf46c3066ceef5b
Konstantin <grahovsky@gmail.com>
2019-03-20 22:16:24 +0300

    #UIImagePickerController #info.plist #Privacy - Camera Usage Description #imageView.contentMode = .scaleAspectFill

--- WatsonSamples/WatsonSamples/ViewController.swift
+++ WatsonSamples/WatsonSamples/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imageView.contentMode = .scaleAspectFill
     }
 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            imageView.image = image
+            
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+        } else {
+            print("There was an error picking the image")
+        }
+        
+    }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
 }
 
