56911ddcd6cd843978c53db700bf92d16752a278
Konstantin <grahovsky@gmail.com>
2019-03-20 22:37:10 +0300

    #VisualRecognitionV3 #get url for image #VisualRecognition(version: version, apiKey: apiKey)

--- WatsonSamples/WatsonSamples/ViewController.swift
+++ WatsonSamples/WatsonSamples/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import VisualRecognitionV3
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    // https://cloud.ibm.com/resources
+    
+    let apiKey = "-8kIFBc5kqNCNWLyo7RqPhQl2-Mb7rXSt0SyKxaOTLfN"
+    let version = "2019-03-20"
     
     let imagePicker = UIImagePickerController()
     
@@ -30,6 +36,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             imageView.image = image
             
             imagePicker.dismiss(animated: true, completion: nil)
+            
+            //get url for image, not used
+            //let imageData = image.jpegData(compressionQuality: 0.01)
+            //let documetsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            //let fileURL = documetsURL.appendingPathComponent("tempImage.jpg")
+            //try? imageData?.write(to: fileURL)
+            
+            let visualRecognition = VisualRecognition(version: version, apiKey: apiKey)
+            visualRecognition.classify(image: image) { (classifiedImages, error) in
+                
+                print(classifiedImages)
+                
+            }
             
         } else {
             print("There was an error picking the image")
